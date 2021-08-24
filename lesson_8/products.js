bba030c2854fc698c928e52b4602230b04fe4135
Alexey <max_89@inbox.ru>
2021-08-24 16:57:49 +0300

    HomeWorkLesson8

---
+++ lesson_8/products.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const products = [
+  {
+    id: 0,
+    image: '0.jpg',
+    name: 'Product 0',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    price: 11.11,
+  },
+  {
+    id: 1,
+    image: '1.jpg',
+    name: 'Product 1',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    price: 22.22,
+  },
+  {
+    id: 2,
+    image: '2.jpg',
+    name: 'Product 2',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    price: 33.33,
+  },
+  {
+    id: 3,
+    image: '3.jpg',
+    name: 'Product 3',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    price: 44.44,
+  },
+  {
+    id: 4,
+    image: '4.jpg',
+    name: 'Product 4',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    price: 55.55,
+  },
+  {
+    id: 5,
+    image: '5.jpg',
+    name: 'Product 5',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    price: 66.66,
+  },
+];
